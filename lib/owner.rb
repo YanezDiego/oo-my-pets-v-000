4a1c0545ce40e4ca4e66ef51e831588b98e3afbe
Diego Yanez <diegosyanez92@gmail.com>
2018-04-17 19:10:38 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -42,10 +42,8 @@ class Owner
     end
 
     def walk_dogs
-      @pets[:dog].each do |walks|
-        if walks
+      @pets[:dog].each do |dog|
           dog.mood = happy
-        end
       end
     end
 
